407b37ac5163076ba48afda945e3c8a2988fe150
ArterioRodrigues <arterio369@gmail.com>
2021-12-16 22:57:20 -0500

    Updates

---
+++ precache-manifest.7217cd466db4d39aab56771e5d0ec050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "546783694ca9594840fa",
+    "url": "/Finalback/static/css/main.73120edb.chunk.css"
+  },
+  {
+    "revision": "546783694ca9594840fa",
+    "url": "/Finalback/static/js/main.54678369.chunk.js"
+  },
+  {
+    "revision": "dbf16a7a1f5d12f53526",
+    "url": "/Finalback/static/js/1.dbf16a7a.chunk.js"
+  },
+  {
+    "revision": "45de306f3f336216fdf0",
+    "url": "/Finalback/static/js/2.45de306f.chunk.js"
+  },
+  {
+    "revision": "082ff2c3d26138bd10c4",
+    "url": "/Finalback/static/js/runtime~main.082ff2c3.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Finalback/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "bd8e840491704f5d57387859bfe7af86",
+    "url": "/Finalback/index.html"
+  }
+];
